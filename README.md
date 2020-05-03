bc0e83884f4fbf72d26ada1c434aa782cc16d594
Kai Yan <kyan2@wpi.edu>
2020-05-03 18:21:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flocking
+
+Developed with Unreal Engine 4
---
+++ Source/Flocking/FlockingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlockingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOCKING_API AFlockingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
